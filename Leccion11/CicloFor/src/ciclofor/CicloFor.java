026ff58d6142a5115a403a652cbff81537fadf60
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-23 19:52:43 -0600

    LECCION 11
    Ciclo for

---
+++ Leccion11/CicloFor/src/ciclofor/CicloFor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclofor;
+
+/**
+ *
+ * @author gerardo
+ */
+public class CicloFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int limite = 10;
+        for (int contador = 0; contador < limite; contador++) {
+            System.out.println("contador = " + contador);
+        }
+    }
+    
+}
