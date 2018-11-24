ad9a5caaff2cccb715022bb0adbf85e5460b6d88
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-23 19:32:50 -0600

    LECCION 7
    Manejo de if-else-if-else

---
+++ Leccion07/EjemploIfElseIf/src/ejemploifelseif/EjemploIfElseIf.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploifelseif;
+
+/**
+ *
+ * @author gerardo
+ */
+public class EjemploIfElseIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //Ejemplo If/Else If/Else
+        int x = 10;
+        if (x == 10) {
+            System.out.print("X igual a 10\n");
+        } else if (x == 20) {
+            System.out.print("X igual a 20\n");
+        } else if (x == 30) {
+            System.out.print("X igual a 30\n");
+        } else {
+            System.out.print("X no es igual ni a 10, ni 20 ni 30\n");
+        }
+    }
+    
+}
