fc2655bf8b000c1db22ee9cba042ac47a39b9e13
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-23 19:30:16 -0600

    LECCION 6
    Manejo if-else

---
+++ Leccion06/EjemploIf/src/ejemploif/EjemploIf.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploif;
+
+/**
+ *
+ * @author gerardo
+ */
+public class EjemploIf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          //Ejemplo If/Else
+        int x = 30;
+        if (x < 20) {
+            System.out.print("X menos que 20\n");
+        } else {
+            System.out.print("X mayor que 20\n");
+        }
+    }
+    
+}
