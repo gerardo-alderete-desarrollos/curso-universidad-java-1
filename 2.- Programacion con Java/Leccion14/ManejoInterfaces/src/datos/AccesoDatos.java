7319a6f7f82241c0618bea0f3328843db413e35d
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-11-27 22:03:43 -0600

    LECCION 14
    Manejo de Interfaces

---
+++ 2.- Programacion con Java/Leccion14/ManejoInterfaces/src/datos/AccesoDatos.java
@@ -0,0 +1,11 @@
+package datos;
+
+import excepciones.*;
+
+public interface AccesoDatos {
+    
+    public abstract void insertar() ;
+    
+    public abstract void listar() ;
+    
+}
---
+++ 2.- Programacion con Java/Leccion14/ManejoInterfaces/src/excepciones/ExcepcionLecturaDatos.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class ExcepcionLecturaDatos extends AccesoDatosEx{
+
+    public ExcepcionLecturaDatos(String mensaje) {
+        super(mensaje);
+    }
+    
+}
